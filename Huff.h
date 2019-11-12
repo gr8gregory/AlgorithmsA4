cbc1d865aa0319d0970422f3478017798286917d
choeksema <56086870+choeksema@users.noreply.github.com>
2019-11-11 22:11:58 -0500

    Update and rename RLE.h to Huff.h

---
+++ Huff.h
@@ -0,0 +1,18 @@
+/*
+Huff.h: Interface of Huffman compression / decompression functions
+
+	Author: Gregory Huras, Caleb Hoeksema, Laura Muhlbauerova
+	Copyright 2019
+*/
+
+
+// Prototypes
+void Menu(void);
+
+void compress(void);
+void decompress(void);
+void audioCompress(void);
+void audioDecompress(void);
+
+
+#pragma once
